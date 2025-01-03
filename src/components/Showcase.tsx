e8cd60667969f433c05859cc146fd4f5d915167c
Cykotech <cykotech.development@yahoo.com>
2025-01-03 14:05:26 -0500

    Remove commented code

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -6,8 +6,6 @@ export default function Showcase() {
       <h2 className="text-xl md:text-xl lg:text-4xl mb-6">
         Community Showcase
       </h2>
-      {/*<div className="outer">*/}
-      {/*  <div className="inner">*/}
       <div className="flex flex-1">
         <div className="outer">
           <div className="inner min-h-full">
@@ -26,7 +24,6 @@ export default function Showcase() {
             </div>
           </div>
         </div>
-        {/*<div className="self-center bg-[rgba(255,255,255,0.1)] bg-clip-border bg-origin-padding bg-scroll bg-repeat shadow-[0px_0px_70px_15px_rgba(0,0,0,0.75)] box-border text-white font-sans font-[cv05_ss01_ss03_zero] text-base leading-6 h-[100px] w-[1px] text-left hidden lg:block"></div>*/}
         <div className="outer">
           <div className="inner min-h-full">
             <div className="flex-1 flex flex-col text-center px-6 py-6">
@@ -44,7 +41,6 @@ export default function Showcase() {
             </div>
           </div>
         </div>
-        {/*<div className="self-center bg-[rgba(255,255,255,0.1)] bg-clip-border bg-origin-padding bg-scroll bg-repeat shadow-[0px_0px_70px_15px_rgba(0,0,0,0.75)] box-border text-white font-sans font-[cv05_ss01_ss03_zero] text-base leading-6 h-[100px] w-[1px] text-left hidden lg:block"></div>*/}
         <div className="outer">
           <div className="inner min-h-full">
             <div className="flex-1 flex flex-col text-center px-6 py-6">
