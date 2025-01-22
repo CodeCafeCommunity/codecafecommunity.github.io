5c8f3d669ea4af57d4f3086947367cd8c71427d3
Cykotech <cykotech.development@yahoo.com>
2025-01-22 03:07:03 -0500

    Convert Brand container in Header to NavLink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router";
 export default function Header() {
   return (
     <header className="sticky top-0 bg-primary/80 backdrop-blur-lg py-4 flex justify-around items-center">
-      <div className="flex gap-2 items-center">
+      <NavLink className="flex gap-2 items-center" to="/">
         <img
           className="w-16"
           src={"/images/coffee_cup.jpg"}
           alt="Code Cafe Logo"
         />
         <h1 className="text-secondary text-3xl">Code Cafe</h1>
-      </div>
+      </NavLink>
       <nav>
         <ul className="flex list-none gap-3 text-secondary">
           <li>
