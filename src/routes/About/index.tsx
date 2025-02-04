3c91c09faa65a90685b84822b64506088b050d23
Cykotech <cykotech.development@yahoo.com>
2025-02-04 13:20:16 -0500

    Create About layout

--- src/routes/About/index.tsx
+++ src/routes/About/index.tsx
@@ -1,7 +1,11 @@
+import Hero from "./Hero.tsx";
+import Staff from "./Staff.tsx";
+
 export default function About() {
   return (
-    <main className="min-h-[25vh] grid place-items-center bg-primary">
-      <h3 className="text-3xl text-secondary">About Code Cafe</h3>
+    <main className="min-h-[80vh]">
+      <Hero />
+      <Staff />
     </main>
   );
 }
