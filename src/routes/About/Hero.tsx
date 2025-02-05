b28185b2877293c43e4c1fcfc07fe0adfc1c6fe0
Cykotech <cykotech.development@yahoo.com>
2025-02-05 02:59:35 -0500

    Create Hero section

---
+++ src/routes/About/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <div className="min-h-[10vh] grid place-items-center bg-background">
+      <h3 className="text-3xl text-primary">About Us</h3>
+      <p className="text-primary">
+        Little snippet about the server & community.
+      </p>
+    </div>
+  );
+}
